020c8bc107ebf1c0ccf6b4b3ef9e88c873b4aedf
Harry Camps <harrycamps@Harrys-MacBook-Air.local>
2022-04-11 15:38:12 +0100

    initial commit, gitignore added

---
+++ Sketch_Diagram_App/Sketch_Diagram_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sketch_Diagram_AppApp.swift
+//  Sketch_Diagram_App
+//
+//  Created by Harry Camps on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Sketch_Diagram_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
